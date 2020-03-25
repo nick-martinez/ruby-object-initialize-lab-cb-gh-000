586d6ec8de725679392e5962465819c59deadaa4
Nick Martinez <nicolas.martinez.244@my.csun.edu>
2020-03-25 18:49:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   def initialize(name, breed)
     @name = name
-    @breed = breed
+    @breed = "Mutt"
   end
 
   def name
@@ -13,7 +13,7 @@ class Dog
   end
 
   def breed
-    @breed = "Mutt"
+    @breed
   end
 
   def breed=(new_breed)
